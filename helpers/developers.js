d807a89c88969cef6878b057d2af0728c3ba4d55
Esteban E. Molin <esteban.emolin@gmail.com>
2017-08-25 15:34:13 +0200

    Fotos en el datamodel

--- helpers/developers.js
+++ helpers/developers.js
@@ -15,24 +15,28 @@ module.exports = {
   getAllDevelopers: function(){
     return {
       'ryan':{
-          name: 'Ryan Johnson',
+          name: 'Ryan Collins',
           favouriteLanguage: 'Java',
-          gender: 'male'
+          gender: 'male',
+          image: 'http://www.cronicadelnoa.com.ar/web/wp-content/uploads/2016/05/john-snow-120x120.jpg'
       },
       'erik':{
-        name: 'Erik Svansson',
+        name: 'Ned Stark',
         favouriteLanguage: 'Python',
-        gender: 'male'
+        gender: 'male',
+        image: 'https://a09.t26.net/taringa/avatares/7/0/0/9/2/0/skate_09/120x120_E49.jpg'
       },
       'emma':{
         name: 'Emma Collins',
         favouriteLanguage: 'Objective-C',
-        gender: 'female'
+        gender: 'female',
+        image: 'http://www.cineymas.com.ar/wp-content/uploads/2016/07/HBO-Game-of-Thrones-Temporada-7-120x120.jpg'
       },
       'olivia':{
         name: 'Olivia Hunttington',
         favouriteLanguage: 'Go',
-        gender: 'female'
+        gender: 'female',
+        image:'http://i2.cdn.cnn.com/cnnnext/dam/assets/170726180510-game-of-thrones-small-11.jpg'
       }
     };
   }
